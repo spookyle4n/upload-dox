426a5499a2f17b1c7fc303532b152268e9918d86
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-09-16 09:45:48 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# coded by wyatt gierer
+# claiming this script as your own will result in a horrible death
+
+print ('''
+
+
+010            001 011      011  110100    10110010010 001101100010
+ 101          101   111    001  011  101       001        110
+  001        110      011 100  1010101100      110        001
+   110  00  000         111   000       110    101        101
+    010 00 111         010   010          001  001        010
+     110 001          100  111             110 111        101
+      
+    
+''')
+use_nouse = str(input(' [?] explode mikeys balls [y/N]: '))
+if use_nouse == 'y':
+    print("okay exploding mikeys balls in ten minutes")
