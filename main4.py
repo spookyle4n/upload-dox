42ca36497559b0081bece67f2204bdf0003d69d5
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-09-16 14:20:31 -0400

    Create main4.py

---
+++ main4.py
@@ -0,0 +1,37 @@
+# coded by wyatt gierer
+# claiming this script as your own will result in a horrible death
+
+print ('''
+
+
+010            001 011      011  110100    10110010010 001101100010
+ 101          101   111    001  011  101       001        110
+  001        110      011 100  1010101100      110        001
+   110  00  000         111   000       110    101        101
+    010 00 111         010   010          001  001        010
+     110 001          100  111             110 111        101
+                     
+		   tiktok:  @jahsehrare
+		   instagram: @spookyle4n
+    
+''')
+
+use_nouse = str(input(' [?] wanna nuke someones balls? [y/N]: '))
+if use_nouse == 'N': 
+    exit()
+if use_nouse == 'y':
+	first_name = input("\n\033[36m[*] First Name: ")
+	last_name = input("\n\033[36m[*] Last Name: ")
+	address = input("\n\033[36m[*] Address: ")
+	date = input("\n\033[36m[*] date: ")
+	time = input("\n\033[36m[*] time: ")
+	use_nouse = str(input("\n\033[36m[?] you sure you want to do this? ? [y/N]: "))
+print(first_name + " " + "[*]")
+print(last_name + " " + "[*]")
+print(address + " " + "[*]")
+print(date + " " + "[*]")
+print(time + " " + "[*]")
+import time
+for i in range(10000):
+    print("nuking" , end=" ", flush=True)
+    time.sleep(0.01)
