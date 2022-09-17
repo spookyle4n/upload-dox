6847da75657d997c7595369508de373d5908bb69
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-09-17 14:47:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,15 @@ print('''\033[31m
    110  00  000          111    000      110      101         101
     010 00 111          010    010         001    001         010
      110  001          100   111            110   111         101     
-
 ''')
-
-print('''\033[1;32m
+print('''              educational purposes only!
 
 		   tiktok:  @jahsehrare
 		   instagram: @spookyle4n
 
 ''')
 
-use_nouse = str(input(' [?] by checking "y" i understand that i am responsible for all actions i partake in [y/N]: '))
+use_nouse = str(input(' [?] for educational purposes only [y/N]: '))
 if use_nouse == 'N': exit()
 
 if use_nouse == 'y':
@@ -29,15 +27,18 @@ if use_nouse == 'y':
     address = input("\n\033[36m[*] Address: ")
     ip = input("\n\033[36m[*] ip: ")
     age = input("\n\033[36m[*] age: ")
-    use_nouse = str(input("\n\033[36m[?] you sure you want to do this? [y/N]: \n"))
-    if use_nouse == 'N': exit()
+    use_nouse = str(input("\n\033[36m[?] use advanced options? [y/N]: "))
+    if use_nouse == 'y':
+        socials = input("\n\033[36m[*] social medias: ")
+        DOT = input("\n\033[36m[*] date of birth: ")
+        number = input("\n\033[36m[*] cellphone number: ")
 
 import time
 
 print('', end=' ', flush=True)
 time.sleep(1)
 
-print("victims first name : " + " " + first_name + " " + "\n")
+print("\nvictims first name : " + " " + first_name + " " + "\n")
 
 print('', end=' ', flush=True)
 time.sleep(1)
@@ -62,15 +63,37 @@ print("age of victim : " + " " + age + " " + "\n")
 print('', end=' ', flush=True)
 time.sleep(1)
 
+if use_nouse == 'y':
+
+    print('', end=' ', flush=True)
+    time.sleep(1)
+
+    print("victims social medias : " + " " + socials + " " + "\n")
+
+    print('', end=' ', flush=True)
+    time.sleep(1)
+
+    print("victims date of birth : " + " " + DOT + " " + "\n")
+
+    print('', end=' ', flush=True)
+    time.sleep(1)
+
+    print("victims cellphone number : " + " " + number + " " + "\n")
+
 use_nouse = str(input('[?] is this information correct? [y/N] : '))
-if use_nouse == 'N': exit()
+if use_nouse == 'N':
+    first_name = input("\n\033[36m[*] First Name: ")
+    last_name = input("\n\033[36m[*] Last Name: ")
+    address = input("\n\033[36m[*] Address: ")
+    ip = input("\n\033[36m[*] ip: ")
+    age = input("\n\033[36m[*] age: ")
 
 print("\n [!] loading script [!] \n")
 
 import time
 
 for i in range(5):
-    print('   .\t', end=' ', flush=True)
+    print('\033[31m   .\t', end=' ', flush=True)
     time.sleep(1)
 
 import time
