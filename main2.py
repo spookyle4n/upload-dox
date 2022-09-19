40de035ae9698e62bc9d62494c833f8bb0ef1b55
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-09-19 09:15:57 -0400

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,108 @@
+# coded by wyatt g#
+
+# claiming this script as your own will result in a horrible death #
+
+print('''\033[31m
+010            001  011      011   110100     10110010010 00110110001
+ 101          101    111    001   011  101        001         110
+  001        110       011 100   1010101100       110         001
+   110  00  000          111    000      110      101         101
+    010 00 111          010    010         001    001         010
+     110  001          100   111            110   111         101     
+''')
+print('''              educational purposes only!
+
+		   tiktok:  @jahsehrare
+		   instagram: @spookyle4n
+''')
+
+use_nouse = str(input(' [?] for educational purposes only [y/N]: '))
+if use_nouse == 'N': exit()
+
+if use_nouse == 'y':
+    first_name = input("\n\033[36m[*] First Name: ")
+    last_name = input("\n\033[36m[*] Last Name: ")
+    address = input("\n\033[36m[*] Address: ")
+    ip = input("\n\033[36m[*] ip: ")
+    age = input("\n\033[36m[*] age: ")
+    use_nouse = str(input("\n\033[36m[?] use advanced options? [y/N]: "))
+    if use_nouse == 'y':
+        socials = input("\n\033[36m[*] social medias: ")
+        DOT = input("\n\033[36m[*] date of birth: ")
+        number = input("\n\033[36m[*] cellphone number: ")
+
+import time
+
+print('', end=' ', flush=True)
+time.sleep(1)
+
+print("\nvictims first name : " + " " + first_name + " " + "\n")
+
+print('', end=' ', flush=True)
+time.sleep(1)
+
+print("victims last name : " + " " + last_name + " " + "\n")
+
+print('', end=' ', flush=True)
+time.sleep(1)
+
+print("victims address : " + " " + address + " " + "\n")
+
+print('', end=' ', flush=True)
+time.sleep(1)
+
+print("victims IP address : " + " " + ip + " " + "\n")
+
+print('', end=' ', flush=True)
+time.sleep(1)
+
+print("victims age : " + " " + age + " " + "\n")
+
+print('', end=' ', flush=True)
+time.sleep(1)
+
+if use_nouse == 'y':
+
+    print('', end=' ', flush=True)
+    time.sleep(1)
+
+    print("victims social medias : " + " " + socials + " " + "\n")
+
+    print('', end=' ', flush=True)
+    time.sleep(1)
+
+    print("victims date of birth : " + " " + DOT + " " + "\n")
+
+    print('', end=' ', flush=True)
+    time.sleep(1)
+
+    print("victims cellphone number : " + " " + number + " " + "\n")
+
+use_nouse = str(input('[?] is this information correct? [y/N] : '))
+if use_nouse == 'N':
+    first_name = input("\n\033[36m[*] First Name: ")
+    last_name = input("\n\033[36m[*] Last Name: ")
+    address = input("\n\033[36m[*] Address: ")
+    ip = input("\n\033[36m[*] ip: ")
+    age = input("\n\033[36m[*] age: ")
+
+print("\n\033[31m [!] loading script [!] \n")
+
+import time
+
+for i in range(5):
+    print('\033[31m   .\t', end=' ', flush=True)
+    time.sleep(1)
+
+import requests
+
+url = 'http://doxbin.com
+query = {'field': value}
+res = requests.post(url, data=query)
+print(first_name + " " + last_name + " " + address + " " + ip + " " + age)
+
+import time
+
+for i in range(10000):
+    print("\033[31m\n nuking [!]", end=' ', flush=True)
+    time.sleep(0.01)
