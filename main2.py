087bbbd9f80fb6428601446392cb2c0265cf252d
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-09-20 09:09:28 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -88,6 +88,7 @@ if use_nouse == 'N':
     ip = input("\n\033[36m[*] ip: ")
     age = input("\n\033[36m[*] age: ")
 
+if use_nouse == 'y':
 print("\n\033[31m [!] loading script [!] \n")
 
 import time
