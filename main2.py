465614b8acc694ce097354f3d4258c1c15dbfc17
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-09-29 14:28:11 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -6,6 +6,7 @@
 
 print('''\033[31m
 
+
 010            001  011      011   110100     10110010010 00110110001  101
  101          101    111    001   011  101        001         110      001
   001        110       011 100   1010101100       110         001      100
@@ -13,8 +14,11 @@ print('''\033[31m
     010 00 111          010    010         001    001         010      
      110  001          100   111            110   111         101      000 
      
+     
 ''')
-print('''              educational purposes only!
+print('''           
+
+educational purposes only!
 
 		   tiktok:  @jahsehrare
 		   instagram: @spookyle4n
