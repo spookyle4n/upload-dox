b4f4955295cbc6fe5e3464d94cb5f7cf7e71c39c
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-07 10:56:22 -0400

    Create main4.py

---
+++ main4.py
@@ -0,0 +1,29 @@
+# coded by wyatt g #
+
+print('''
+
+    111111
+    111111
+    111111
+    111111
+    111111
+    111111
+    111111
+    111111111111
+    111111111111
+    
+''')
+
+print('''           
+
+           educational purposes only!
+
+		   tiktok:  @jahsehrare
+		   instagram: @spookyle4n
+''')
+
+dox = str(input(' [?] open doxbin.org [y/n]: '))
+import webbrowser
+if dox == 'y': webbrowser.open('http://www.doxbin.com')
+
+if dox == 'n': exit()
