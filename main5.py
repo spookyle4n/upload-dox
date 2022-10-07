2fec67888c2cdb13fe2213a7857116528b239ec9
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-07 14:30:22 -0400

    Create main5.py

---
+++ main5.py
@@ -0,0 +1,35 @@
+import time
+
+print('''
+
+\033[31m       ____            _     _       
+\033[32m      |  _ \  _____  _| |__ (_)_ __  
+\033[33m      | | | |/ _ \ \/ / '_ \| | '_ \ 
+\033[34m      | |_| | (_) >  <| |_) | | | | |
+\033[35m      |____/ \___/_/\_\_.__/|_|_| |_|
+
+''')
+
+first_name = input("First Name: ")
+
+last_name = input("Last Name: ")
+
+address = input("Address: ")
+
+ip = input("ip: ")
+
+age = input("age: ")
+
+print("victims first name : " + " " + first_name + " " + "\n")
+time.sleep(0.5)
+
+print("victims last name : " + " " + last_name + " " + "\n")
+time.sleep(0.5)
+
+print("victims address : " + " " + address + " " + "\n")
+time.sleep(0.5)
+
+print("victims IP address : " + " " + ip + " " + "\n")
+time.sleep(0.5)
+
+print("victims age : " + " " + age + " " + "\n")
